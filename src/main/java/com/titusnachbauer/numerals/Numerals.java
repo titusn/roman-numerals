7331bd7195549d33536fbd4d2074232e650badc3
titusn <mail@titusnachbauer.com>
2021-02-13 17:30:20 +0100

    Works up to 4

--- src/main/java/com/titusnachbauer/numerals/Numerals.java
+++ src/main/java/com/titusnachbauer/numerals/Numerals.java
@@ -3,7 +3,11 @@ package com.titusnachbauer.numerals;
 public class Numerals {
     public String convert(int i) {
         StringBuilder result = new StringBuilder();
-        for (;i >=  1;i--) {
+        for (;i >= 1;i--) {
+            if (i % 4 == 0) {
+                result.append("IV");
+                break;
+            }
             result.append("I");
         }
         return result.toString();
--- src/test/java/NumeralsTest.java
+++ src/test/java/NumeralsTest.java
@@ -9,5 +9,7 @@ public class NumeralsTest {
         Numerals numerals = new Numerals();
         assertEquals("I", numerals.convert(1));
         assertEquals("II", numerals.convert(2));
+        assertEquals("III", numerals.convert(3));
+        assertEquals("IV", numerals.convert(4));
     }
 }
