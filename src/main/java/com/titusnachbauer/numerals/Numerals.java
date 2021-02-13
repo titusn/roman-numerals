191c29bd544afb659d57d3900e3ab07cbd9fe736
titusn <mail@titusnachbauer.com>
2021-02-13 19:44:35 +0100

    Refactor to use reverse sorted map
    This should be slightly more readable and, although we do not expect anyone to extend Roman
    numerals soon, also makes it easier to extend.

--- src/main/java/com/titusnachbauer/numerals/Numerals.java
+++ src/main/java/com/titusnachbauer/numerals/Numerals.java
@@ -1,18 +1,31 @@
 package com.titusnachbauer.numerals;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class Numerals {
-    private final String[] ROMAN = {"M","CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    private final int[] ARABIC = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private final SortedMap<Integer, String> ARABIC_TO_ROMAN = new TreeMap<>(Collections.reverseOrder()) {{
+        put(1000, "M");
+        put(900, "CM");
+        put(500, "D");
+        put(400, "CD");
+        put(100, "C");
+        put(90, "XC");
+        put(50, "L");
+        put(40, "XL");
+        put(10, "X");
+        put(9, "IX");
+        put(5, "V");
+        put(4, "IV");
+        put(1, "I");
+    }};
 
     public String convert(int number) {
         StringBuilder result = new StringBuilder();
         int remaining = number;
-        for (int i = 0; i < ARABIC.length; i++) {
-            while (remaining >= ARABIC[i]) {
-                result.append(ROMAN[i]);
-                remaining -= ARABIC[i];
+        for (Map.Entry<Integer, String> entry  : ARABIC_TO_ROMAN.entrySet()) {
+            while (remaining >= entry.getKey()) {
+                result.append(entry.getValue());
+                remaining -= entry.getKey();
             }
         }
         return result.toString();
