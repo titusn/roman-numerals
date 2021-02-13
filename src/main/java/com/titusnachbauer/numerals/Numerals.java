4c85d7a99e3298a771b58fd7715fb60ed61e380a
titusn <mail@titusnachbauer.com>
2021-02-13 18:57:59 +0100

    Refactor to loop using arrays

--- src/main/java/com/titusnachbauer/numerals/Numerals.java
+++ src/main/java/com/titusnachbauer/numerals/Numerals.java
@@ -7,16 +7,11 @@ public class Numerals {
     public String convert(int number) {
         StringBuilder result = new StringBuilder();
         int remaining = number;
-        if (remaining >= 9) {
-            result.append("IX");
-            remaining -= 9;
-        } else if (remaining >= 5) {
-            result.append("V");
-            remaining -= 5;
-        }
-        else if (remaining >= 4) {
-            result.append("IV");
-            remaining -= 4;
+        for (int i = 0; i < ARABIC.length; i++) {
+            if (remaining >= ARABIC[i]) {
+                result.append(ROMAN[i]);
+                remaining -= ARABIC[i];
+            }
         }
         result.append("I".repeat(Math.max(0, remaining)));
         return result.toString();
